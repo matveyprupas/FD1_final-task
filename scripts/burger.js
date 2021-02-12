45db8dc89d6b0ad6a21f687daa51b8784b2cb00e
matveyprupas <matveyprupas@gmail.com>
2021-02-12 23:02:37 +0200

    fix: finish burger menu feat: start order form

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,7 +1,15 @@
 function burgerMenu () {
     let top = document.querySelector("header.header").clientHeight;
-
     document.body.classList.toggle("-burger-activated");
-
     document.querySelector("nav.header__nav").style.top = `${top}px`;
+}
+
+function openList() {
+    document.body.classList.toggle("-list-activated");
+}
+
+function openForm() {
+    let top = document.querySelector("header.header").clientHeight;
+    document.body.classList.toggle("-form-opened");
+    document.querySelector(".order-form").style.top = `${top}px`;
 }
